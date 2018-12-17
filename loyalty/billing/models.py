7ed5356b094d8fbff2cb668eacf423614d0660e6
Damidara16 <33613023+Damidara16@users.noreply.github.com>
2018-12-17 07:56:42 -0500

    Update models.py

--- loyalty/billing/models.py
+++ loyalty/billing/models.py
@@ -1,1 +1,21 @@
 from django.db import models
+
+class OnCycle(models.Model):
+     active = models.BooleanField(default=True)
+     unactive = models.BooleanField(default=False)
+     api_key = models.CharField(max_length=10)
+
+     def save(self, *args, **kwargs):
+          if self.active = False:
+               self.unactive = True
+          return super(OnCycle).save(*args, **kwargs)
+
+     def reactive(self):
+          if self.unactive = True:
+               self.active = True
+               self.save()
+
+     def deactivate(self):
+          if self.active = False:
+               self.unactive = True
+               self.save()
