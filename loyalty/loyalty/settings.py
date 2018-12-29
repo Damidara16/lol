c00a0855953660d51e9ab5d69b7689edde2a42e8
simeon <sajala8624@gmail.com>
2018-12-29 01:18:53 -0500

    'customer'

--- loyalty/loyalty/settings.py
+++ loyalty/loyalty/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'store_user',
     'customer_user',
     'content',
+    'rest_framework.authtoken'
+
 ]
 
 MIDDLEWARE = [
